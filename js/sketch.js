4ce145d3d4beb1050072c8315a6f90c0b109c4e6
yyoona <yoona.yang@aalto.fi>
2022-10-06 20:04:22 +0300

    data

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,35 @@
+let data;
+
+function preload(){
+  data = loadTable('InformationDesign Dataset_week1.csv', 'csv', 'header');
+}
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    // background(255);
+  createCanvas(windowWidth, windowHeight);
+  background(200)
+  noLoop();
     
    }
    
 function draw() {
-    fill (0);
-    noStroke();
-    arc(100,100,100,100, 0, 200);
+  if(data){
+    
+    let numRows = data.getRowCount();
+    
+    let sunrise_mins = data.getColumn('Sunrise_Mins')
+    print(sunrise_mins);
+    
+    for (let i = 0; i <numRows; i++){
+      
+      let x = 10;
+      let y = 10 + (i*100);
+      let w = 50;
+      let h = sunrise_mins[i]*0.1;
+      
+      rect(x,y,w,h);
+      
+    }
+    
+  }
+
 }
