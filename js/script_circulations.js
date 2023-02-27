93b8de51a738b1daa5bebfb59f5f82b39765e65e
yyoona <yoona.per@gmail.com>
2023-02-27 03:32:45 +0200

    update

--- js/script_circulations.js
+++ js/script_circulations.js
@@ -3,6 +3,7 @@ const spanElement = document.querySelector("#slideshow_span")
 let globalIndex = 0;
 
 // list of paths to the images
+// let images = ["../Artistic Practice/Circulations/Circulations_00.jpg", "../Artistic Practice/Circulations/Circulations_01.jpg", "../Artistic Practice/Circulations/Circulations_02.jpg", "../Artistic Practice/Circulations/Circulations_03.jpg", "../Artistic Practice/Circulations/Circulations_04.jpg", "../Artistic Practice/Circulations/Circulations_05.jpg", "../Artistic Practice/Circulations/Circulations_06.jpg", "../Artistic Practice/Circulations/Circulations_07.jpg", "../Artistic Practice/Circulations/Circulations_08.jpg", "../Artistic Practice/Circulations/Circulations_09.jpg", "../Artistic Practice/Circulations/Circulations_10.jpg"];
 let images = ["../Artistic Practice/Circulations/Circulations_00.jpg", "../Artistic Practice/Circulations/Circulations_01.jpg", "../Artistic Practice/Circulations/Circulations_02.jpg", "../Artistic Practice/Circulations/Circulations_03.jpg", "../Artistic Practice/Circulations/Circulations_04.jpg", "../Artistic Practice/Circulations/Circulations_05.jpg", "../Artistic Practice/Circulations/Circulations_06.jpg", "../Artistic Practice/Circulations/Circulations_07.jpg", "../Artistic Practice/Circulations/Circulations_08.jpg", "../Artistic Practice/Circulations/Circulations_09.jpg", "../Artistic Practice/Circulations/Circulations_10.jpg"];
 
 
